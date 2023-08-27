f48d9cdef5f862da6f6cace3369be1baea9743c7
Prathamesh <prathameshdhasade99@gmail.com>
2023-08-27 18:31:30 +0530

    new classes added

--- src/testng1/ThirdTest.java
+++ src/testng1/ThirdTest.java
@@ -11,48 +11,74 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class ThirdTest {
-	@BeforeSuite
-	public void BS() {
-		System.out.println("This code executes before the suite ");
-	}
 
-	@AfterSuite
-	public void AS() {
-		System.out.println("This code executes after the suite ");
-	}
-
-	@BeforeTest
-	public void BT() {
-		System.out.println("This code executes before the test ");
-	}
-
-	@AfterTest
-	public void AT() {
-		System.out.println("This code executes after the test ");
-	}
-
-	@BeforeClass
-	public void BC() {
-		System.out.println("This code executes before the class ");
-	}
-
-	@AfterClass
-	public void AC() {
-		System.out.println("This code executes after the class ");
-	}
-
-	@BeforeMethod
-	public void BM() {
-		System.out.println("This code executes before the method ");
-	}
-
-	@AfterMethod
-	public void AM() {
-		System.out.println("This code executes after the method ");
+	@Test
+	public void testCase1() {
+		System.out.println("This is Test Case 1");
 	}
 
 	@Test
-	public void doremon() {
-		System.out.println("I am Doremon");
+	public void testCase2() {
+		System.out.println("This is Test Case 2");
+	}
+
+	// methods under this annotation will be executed prior to each method in each
+	// test case.
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("This will execute before every Test Method");
+	}
+
+	// methods under this annotation will be executed after each method in each test
+	// case.
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("This will execute after every Test Method");
+	}
+
+	// The annotated method will be run before the first test method in the current
+	// class is invoked. It runs only once per class.
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("This will execute before the Class");
+	}
+
+	// The annotated method will be run after all the test methods in the current
+	// class have been run.
+	@AfterClass
+	public void afterClass() {
+		System.out.println("This will execute after the Class");
+	}
+
+	// methods under this annotation will be executed prior to the first test case
+	// in the TestNG file.
+	// The annotated method will run before any test method belonging to the classes
+	// inside the <test> tag is run.
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("This method will be executed before each test case");
+	}
+
+	// methods under this annotation will be executed after all test cases in the
+	// TestNG file are executed.
+	// The annotated method will run after all the test methods belonging to the
+	// classes inside the <test> tag have run.
+	@AfterTest
+	public void afterTest() {
+		System.out.println("This method will be executed after each test case");
+	}
+
+	// The annotated method will run only once before all tests in this suite have
+	// run.
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("This will execute before the Test Suite");
+	}
+
+	// The annotated method will run only once after all tests in this suite have
+	// run.
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("This will execute after the Test Suite");
 	}
 }
